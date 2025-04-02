f12896dd25777fa532db42dc23f5136538d10ace
Jean Bryant Figueroa <jbster559@gmail.com>
2025-04-02 10:26:36 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+//Author : Jean Bryant Figueroa
+//Course : COMP003B
+//Fauculty : Johnathan Cruz
+//Purpose : 3.7 Coding Assignment: MVC Routing, Model Binding, and Validation
+
+namespace COMP003B.Assignment3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
